fb9d62e27782c11c1a390ebb661eac1743f7712a
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-11-08 20:41:33 +0530

    url added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const URL = "mongodb+srv://Iswarya:Geetha71@cluster0.stxvr2g.mongodb.net/?retryW
 
 app.use(
   cors({
-    origin: "https://wonderful-pasca-1dd86e.netlify.app/",
+    origin: "https://wonderful-pasca-1dd86e.netlify.app",
   })
 );
 
