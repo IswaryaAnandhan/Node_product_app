a445de5e092e019ab5652b79910d6293c9336681
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-11-19 20:30:49 +0530

    added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express").Router;
 const cors = require("cors");
 const mongodb = require("mongodb");
 const mongoclient = mongodb.MongoClient;
